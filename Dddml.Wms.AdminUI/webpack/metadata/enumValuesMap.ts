a2bc0e3230282e36c335853406a91198c17eb768
wubuku <yangjiefeng@gmail.com>
2016-10-13 18:45:22 +0800

    增加了一个 T4 脚本。

---
+++ Dddml.Wms.AdminUI/webpack/metadata/enumValuesMap.ts
@@ -0,0 +1,18 @@
+export const ENUM_VALUES_MAP = [
+  {
+    "name": "DocumentAction",
+    "values": [
+		
+      "Draft": "Draft",
+		
+      "Complete": "Complete",
+		
+      "Void": "Void",
+		
+      "Close": "Close",
+		
+      "Reverse": "Reverse",
+    ]
+  }
+]
+
