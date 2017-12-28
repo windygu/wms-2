c846dc67a7ed822d9246bc571880fbcdb51d3fc6
wubuku <yangjiefeng@gmail.com>
2017-12-28 12:03:01 +0800

    修改代码目录结构。

--- Dddml.Wms.Common/Support/AuthorizationUtils.cs
+++ Dddml.Wms.Common/Support/AuthorizationUtils.cs
@@ -2,13 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Dddml.Wms.Support
 {
     public static class AuthorizationUtils
     {
-
-     
     }
 }
