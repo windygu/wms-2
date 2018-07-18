d78bdc78343b79bcaac9c913ea07e224d14c16ff
wubuku <yangjiefeng@gmail.com>
2018-07-18 15:40:03 +0800

    增加了获取单号的服务。

---
+++ java/Dddml.Wms.JavaRestful/src/generated/java/org/dddml/wms/restful/resource/IdServiceResource.java
@@ -0,0 +1,31 @@
+package org.dddml.wms.restful.resource;
+
+import java.util.*;
+import javax.servlet.http.*;
+import javax.validation.constraints.*;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import org.dddml.support.criterion.*;
+import org.dddml.wms.domain.*;
+import org.dddml.wms.specialization.*;
+import org.dddml.wms.domain.meta.*;
+import org.dddml.wms.domain.service.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RequestMapping(path = "IdService", produces = MediaType.APPLICATION_JSON_VALUE)
+@RestController
+public class IdServiceResource {
+
+    @Autowired
+    private IdApplicationService idApplicationService;
+
+    @PostMapping("GetDocumentNumber")
+    public String getDocumentNumber(@RequestBody IdServiceCommandDtos.GetDocumentNumberDto requestContent)
+    {
+        return idApplicationService.when(requestContent.toGetDocumentNumber());
+    }
+
+}
+
---
+++ java/Dddml.Wms.JavaServices/src/main/java/org/dddml/wms/domain/service/IdApplicationServiceImpl.java
@@ -0,0 +1,34 @@
+package org.dddml.wms.domain.service;
+
+import org.dddml.wms.domain.documenttype.DocumentTypeIds;
+import org.dddml.wms.specialization.hibernate.TableIdGenerator;
+import org.dddml.wms.specialization.ApplicationContext;
+
+/**
+ * Created by yangjiefeng on 2018/7/18.
+ */
+public class IdApplicationServiceImpl implements IdApplicationService  {
+
+    @Override
+    public String when(IdServiceCommands.GetDocumentNumber c) {
+        String docType = c.getDocumentType();
+        TableIdGenerator idGen = ApplicationContext.current.get(TableIdGenerator.class);
+        String seqId = idGen.getNextId().toString();
+        String docTypePrefix = docType.substring(0, 1).toUpperCase();
+        if (DocumentTypeIds.IN.equalsIgnoreCase(docType)) {
+            docTypePrefix = "R";
+        } else if (DocumentTypeIds.OUT.equalsIgnoreCase(docType)) {
+            docTypePrefix = "C";
+        } else if (DocumentTypeIds.SHIPMENT.equalsIgnoreCase(docType)) {
+            docTypePrefix = "S";
+        } else if (DocumentTypeIds.MOVEMENT.equalsIgnoreCase(docType)) {
+            docTypePrefix = "M";
+        } else if (DocumentTypeIds.PHYSICAL_INVENTORY.equalsIgnoreCase(docType)) {
+            docTypePrefix = "P";
+        } else if (DocumentTypeIds.)
+        //todo more type???
+
+        return docTypePrefix + seqId;
+    }
+
+}
