b17ffb1d20520a48dd7085cb438086929dc15e41
wubuku <yangjiefeng@gmail.com>
2018-07-19 11:05:34 +0800

    修改代码。

--- java/Dddml.Wms.JavaServices/src/main/java/org/dddml/wms/domain/attributesetinstance/AttributeSetInstanceUtils.java
+++ java/Dddml.Wms.JavaServices/src/main/java/org/dddml/wms/domain/attributesetinstance/AttributeSetInstanceUtils.java
@@ -5,6 +5,7 @@ import org.dddml.wms.domain.service.AttributeSetService;
 import org.dddml.wms.specialization.DomainError;
 import org.dddml.wms.specialization.ReflectUtils;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -15,13 +16,14 @@ public class AttributeSetInstanceUtils {
     public static String createAttributeSetInstance(
             AttributeSetService attributeSetService, AttributeSetInstanceApplicationService attrSetInstApplicationService,
                                                     String attrSetId, Map<String, Object> attrSetInstDict) {
+        Map<String, String> nameDict = null;
         if (attrSetId == null) {
-            return null;
+            nameDict = new HashMap<>();
+        } else {
+            nameDict = attributeSetService.getPropertyExtensionFieldDictionary(attrSetId);
         }
-        Map<String, String> nameDict = attributeSetService.getPropertyExtensionFieldDictionary(attrSetId);
-
         AttributeSetInstanceCommand.CreateAttributeSetInstance createAttrSetInst = new AbstractAttributeSetInstanceCommand.SimpleCreateAttributeSetInstance();
-        createAttrSetInst.setAttributeSetId(attrSetId);
+        createAttrSetInst.setAttributeSetId(attrSetId == null ? "*" : attrSetId);
         for (Map.Entry<String, Object> kv : attrSetInstDict.entrySet()) {
             String fname = nameDict.containsKey(kv.getKey()) ? nameDict.get(kv.getKey()) : kv.getKey();
             // createAttrSetInst.AirDryMetricTon = (decimal)kv.Value;
