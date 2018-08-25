054d58cfb7a0ac9dac784c5225eba091b07007ad
wubuku <yangjiefeng@gmail.com>
2018-08-25 14:06:26 +0800

    修改测试代码。

--- java/Dddml.Wms.JavaRestful/src/test/java/org/dddml/wms/restful/tests/InitInventoryItems.java
+++ java/Dddml.Wms.JavaRestful/src/test/java/org/dddml/wms/restful/tests/InitInventoryItems.java
@@ -1,20 +1,29 @@
 package org.dddml.wms.restful.tests;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.dddml.wms.domain.product.CreateOrMergePatchProductDto;
 import org.dddml.wms.restful.resource.ImportServiceResource;
 import org.dddml.wms.security.JwtUser;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -101,7 +110,7 @@ public class InitInventoryItems {
         claims.put("role", u.getRole());
         //claims.put("userId", u.getId());
 
-        String secret = "XXXXXX";
+        String secret = "xxxxxxx";
         String token = Jwts.builder()
                 .setClaims(claims)
                 .signWith(SignatureAlgorithm.HS512, secret)
@@ -109,6 +118,42 @@ public class InitInventoryItems {
 
         System.out.println(token);
         return token;
+    }
+
+    /**
+     * 从认证服务器上获取一个 Token（知道用户名和密码）
+     * @return
+     */
+    private static String getJwtTokenRemote() {
+        String authUrl = "http://localhost:8089/iam/oauth2/token";
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        try {
+            HttpPost httpPost = new HttpPost(authUrl);
+            httpPost.setHeader("ACCEPT", "application/json");
+            List<NameValuePair> pairs = new ArrayList<>();
+            pairs.add(new BasicNameValuePair("grant_type", "password"));
+            pairs.add(new BasicNameValuePair("client_id", "malls"));
+            pairs.add(new BasicNameValuePair("username", "admin0"));
+            pairs.add(new BasicNameValuePair("password", "admin0"));
+            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(pairs);
+            httpPost.setEntity(entity);
+            HttpResponse response = client.execute(httpPost);
+            //Assert.assertEquals(true, String.valueOf(response.getStatusLine().getStatusCode()).startsWith("20"));
+            System.out.println(response.getStatusLine().getStatusCode());
+            String jsonStr = readString(response);
+            System.out.println(jsonStr);
+            client.close();
+            JSONObject jsonObject = JSONObject.parseObject(jsonStr);
+            String token = (String)jsonObject.get("access_token");
+            if (token == null) {
+                throw new NullPointerException("access_token is null.");
+            }
+            System.out.println(token);
+            return token;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
     }
 
     private static String appendUrl(String url, String component) {
@@ -147,4 +192,14 @@ public class InitInventoryItems {
         return client.execute(httpPut);
     }
 
+    private static String readString(HttpResponse response) throws IOException {
+        InputStream inputStream = response.getEntity().getContent();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+        return sb.toString();
+    }
 }
