135ae6ae11eb4d2949b050269f536c29dd70e2fc
wubuku <yangjiefeng@gmail.com>
2018-09-03 22:28:51 +0800

    增加了盘点单添加行项的方法实现。

---
+++ java/Dddml.Wms.JavaServices/src/main/java/org/dddml/wms/domain/physicalinventory/PhysicalInventoryApplicationServiceImpl.java
@@ -0,0 +1,134 @@
+package org.dddml.wms.domain.physicalinventory;
+
+import org.dddml.wms.domain.attributesetinstance.AttributeSetInstanceApplicationService;
+import org.dddml.wms.domain.attributesetinstance.AttributeSetInstanceState;
+import org.dddml.wms.domain.attributesetinstance.AttributeSetInstanceUtils;
+import org.dddml.wms.domain.inventoryitem.InventoryItemApplicationService;
+import org.dddml.wms.domain.inventoryitem.InventoryItemId;
+import org.dddml.wms.domain.inventoryitem.InventoryItemIds;
+import org.dddml.wms.domain.product.ProductApplicationService;
+import org.dddml.wms.domain.product.ProductState;
+import org.dddml.wms.domain.service.AttributeSetService;
+import org.dddml.wms.specialization.ApplicationContext;
+import org.dddml.wms.specialization.EventStore;
+import org.dddml.wms.specialization.IdGenerator;
+import org.dddml.wms.specialization.hibernate.TableIdGenerator;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by yangjiefeng on 2018/9/3.
+ */
+public class PhysicalInventoryApplicationServiceImpl extends AbstractPhysicalInventoryApplicationService.SimplePhysicalInventoryApplicationService {
+
+    static final String ATTRIBUTE_SET_INSTANCE_ID_KEY = "attributeSetInstanceId";
+
+    ProductApplicationService getProductApplicationService() {
+        return (ProductApplicationService) ApplicationContext.current.get("productApplicationService");
+    }
+
+    AttributeSetInstanceApplicationService getAttributeSetInstanceApplicationService() {
+        return (AttributeSetInstanceApplicationService) ApplicationContext.current.get("attributeSetInstanceApplicationService");
+    }
+
+    AttributeSetService getAttributeSetService() {
+        return (AttributeSetService) ApplicationContext.current.get("attributeSetService");
+    }
+
+    InventoryItemApplicationService getInventoryItemApplicationService() {
+        return (InventoryItemApplicationService) ApplicationContext.current.get("inventoryItemApplicationService");
+    }
+
+    private IdGenerator<Long, Object, Object> seqIdGenerator = new TableIdGenerator();
+
+    public IdGenerator<Long, Object, Object> getSeqIdGenerator() {
+        return seqIdGenerator;
+    }
+
+    public void setSeqIdGenerator(IdGenerator<Long, Object, Object> seqIdGenerator) {
+        this.seqIdGenerator = seqIdGenerator;
+    }
+
+    public PhysicalInventoryApplicationServiceImpl(EventStore eventStore, PhysicalInventoryStateRepository stateRepository, PhysicalInventoryStateQueryRepository stateQueryRepository) {
+        super(eventStore, stateRepository, stateQueryRepository);
+    }
+
+    @Transactional
+    @Override
+    public void when(PhysicalInventoryCommands.CountItem c) {
+        Map<String, Object> attributeSetInstance = new HashMap<>(c.getAttributeSetInstance());
+        String attributeSetInstanceId = (String) attributeSetInstance.get(ATTRIBUTE_SET_INSTANCE_ID_KEY);
+        ProductState productState = getProductState(c.getProductId());
+        if (attributeSetInstanceId == null || attributeSetInstanceId.trim().isEmpty()) {
+            attributeSetInstance.remove(ATTRIBUTE_SET_INSTANCE_ID_KEY);
+            if (attributeSetInstance.size() > 0) {
+                attributeSetInstanceId = AttributeSetInstanceUtils.createAttributeSetInstance(
+                        getAttributeSetService(), getAttributeSetInstanceApplicationService(),
+                        productState.getAttributeSetId(), attributeSetInstance);
+            }
+        } else {
+            AttributeSetInstanceState attrSetInstState = getAttributeSetInstanceApplicationService().get(attributeSetInstanceId);
+            if (attrSetInstState == null) {
+                throw new NullPointerException(
+                        String.format("AttributeSetInstanceState NOT found. AttributeSetInstanceState Id.: %1$s",
+                                attributeSetInstanceId));
+            }
+        }
+        String notNullAttrSetInstId = (attributeSetInstanceId == null || attributeSetInstanceId.isEmpty())
+                ? InventoryItemIds.EMPTY_ATTRIBUTE_SET_INSTANCE_ID : attributeSetInstanceId;
+        attributeSetInstance.put(ATTRIBUTE_SET_INSTANCE_ID_KEY, notNullAttrSetInstId);
+        c.setAttributeSetInstance(attributeSetInstance);
+        super.when(c);
+    }
+
+    private ProductState getProductState(String productId) {
+        ProductState prdState = getProductApplicationService().get(productId);
+        if (prdState == null) {
+            throw new IllegalArgumentException(String.format("Product NOT found. Product Id.: %1$s", productId));
+        }
+        return prdState;
+    }
+
+    public PhysicalInventoryAggregate getPhysicalInventoryAggregate(PhysicalInventoryState state)
+    {
+        return new PhysicalInventoryAggregateImpl(state);
+    }
+
+    public class PhysicalInventoryAggregateImpl extends AbstractPhysicalInventoryAggregate.SimplePhysicalInventoryAggregate {
+
+        public PhysicalInventoryAggregateImpl(PhysicalInventoryState state) {
+            super(state);
+        }
+
+        @Override
+        public void countItem(String locatorId, String productId, java.util.Map<String, Object> attributeSetInstance, BigDecimal countedQuantity, Long version, String commandId, String requesterId) {
+            String attributeSetInstanceId = (String) attributeSetInstance.get(ATTRIBUTE_SET_INSTANCE_ID_KEY);
+            if (attributeSetInstanceId == null || attributeSetInstanceId.trim().isEmpty()) {
+                throw new NullPointerException("The value of 'attributeSetInstanceId' is null.");
+            }
+            String notNullAttrSetInstId = attributeSetInstanceId;
+            InventoryItemId inventoryItemId = new InventoryItemId(productId, locatorId, notNullAttrSetInstId);
+            PhysicalInventoryLineState lineState = getState().getPhysicalInventoryLines().get(inventoryItemId, false, true);
+
+            PhysicalInventoryEvent.PhysicalInventoryStateMergePatched e = newPhysicalInventoryStateMergePatched(version, commandId, requesterId);
+            if (lineState != null) {
+                PhysicalInventoryLineEvent.PhysicalInventoryLineStateMergePatched lineE = e.newPhysicalInventoryLineStateMergePatched(inventoryItemId);
+                lineE.setCountedQuantity(countedQuantity);
+                e.addPhysicalInventoryLineEvent(lineE);
+                apply(e);
+            } else {
+                PhysicalInventoryLineEvent.PhysicalInventoryLineStateCreated lineE = e.newPhysicalInventoryLineStateCreated(inventoryItemId);
+                lineE.setLineNumber(getSeqIdGenerator().getNextId().toString());
+                lineE.setCountedQuantity(countedQuantity);
+                lineE.setBookQuantity(BigDecimal.valueOf(0));
+                e.addPhysicalInventoryLineEvent(lineE);
+                apply(e);
+            }
+        }
+
+    }
+
+}
---
+++ java/Dddml.Wms.JavaServices/src/test/java/org/dddml/wms/PhysicalInventoryTests.java
@@ -0,0 +1,107 @@
+package org.dddml.wms;
+
+import org.dddml.wms.domain.attribute.*;
+import org.dddml.wms.domain.attributeset.*;
+import org.dddml.wms.domain.physicalinventory.*;
+import org.dddml.wms.domain.product.*;
+import org.dddml.wms.domain.uom.UomIds;
+import org.dddml.wms.specialization.ApplicationContext;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Created by yangjiefeng on 2018/9/3.
+ */
+public class PhysicalInventoryTests {
+
+    private ProductApplicationService productApplicationService;
+
+    private AttributeSetApplicationService attributeSetApplicationService;
+
+    private AttributeApplicationService attributeApplicationService;
+
+    private PhysicalInventoryApplicationService physicalInventoryApplicationService;
+
+    public void setUp() {
+        attributeApplicationService = (AttributeApplicationService) ApplicationContext.current.get("attributeApplicationService");
+        attributeSetApplicationService = (AttributeSetApplicationService) ApplicationContext.current.get("attributeSetApplicationService");
+        productApplicationService = (ProductApplicationService) ApplicationContext.current.get("productApplicationService");
+        physicalInventoryApplicationService = (PhysicalInventoryApplicationService) ApplicationContext.current.get("physicalInventoryApplicationService");
+    }
+
+    public void testCreatePhysicalInventoryAndAddLines() {
+
+        String piId = (new Date().getTime()) + "" + UUID.randomUUID().hashCode();
+        createTestPhysicalInventory(piId);
+
+        PhysicalInventoryState physicalInventoryState = physicalInventoryApplicationService.get(piId);
+
+        // //////////////////////////// Create Product /////////////////////////////////////
+        String prdId_1 = new java.util.Date().getTime() + "" + UUID.randomUUID().hashCode();
+        ProductCommand.CreateProduct prd = createFPProduct_1(prdId_1);
+        // /////////////////////////////////
+
+        Long piVersion = physicalInventoryState.getVersion();
+
+        PhysicalInventoryCommands.CountItem countItem_1 = createTestCountItem(prdId_1, piId, piVersion);
+
+        physicalInventoryApplicationService.when(countItem_1);
+        piVersion++;
+
+    }
+
+    PhysicalInventoryCommands.CountItem createTestCountItem(String productId, String piId, Long physicalInventoryVersion) {
+        PhysicalInventoryCommands.CountItem countItem_1 = new PhysicalInventoryCommands.CountItem();
+        countItem_1.setDocumentNumber(piId);
+        countItem_1.setVersion(physicalInventoryVersion);
+        countItem_1.setProductId(productId);
+        countItem_1.setLocatorId(InOutTests.TEST_LOCATOR_ID_1);
+        // ////////////////////////  Atttribute Set Instance /////////////////////////////////
+        Map<String, Object> attrSetInst_1 = new HashMap<>();
+        attrSetInst_1.put("SerialNumber", UUID.randomUUID().toString());
+        //attrSetInst_1.Add("WidthInch", (decimal)17.75);
+        //attrSetInst_1.Add("DiameterInch", 48.00);
+        //attrSetInst_1.Add("WeightLbs", (decimal)1678);
+        //attrSetInst_1.put("WeightKg", BigDecimal.valueOf(773.233));
+        //attrSetInst_1.Add("AirDryWeightLbs", (decimal)1705.682);
+        attrSetInst_1.put("airDryWeightKg", BigDecimal.valueOf(773.684));
+        //attrSetInst_1.Add("AirDryMetricTon", (decimal)0.774);
+        //attrSetInst_1.Add("RollCnt", 2);
+        //attrSetInst_1.Add("AirDryPct", (decimal)101.650);
+        attrSetInst_1.put("statusIds", new String[] {"DAMAGE_WRAPPER", "DAMAGE_EDGE"});
+        // /////////////////////////////////////////////////////////////////////////////////////
+        countItem_1.setAttributeSetInstance(attrSetInst_1);
+        countItem_1.setCountedQuantity(BigDecimal.valueOf(773.5));
+        countItem_1.setCommandId(UUID.randomUUID().toString());
+        return countItem_1;
+    }
+
+    void createTestPhysicalInventory(String piId) {
+        PhysicalInventoryCommand.CreatePhysicalInventory createPhysicalInventory = new AbstractPhysicalInventoryCommand.SimpleCreatePhysicalInventory();
+        createPhysicalInventory.setDocumentNumber(piId);
+        createPhysicalInventory.setWarehouseId(InOutTests.TEST_WAREHOUSE_ID);
+        createPhysicalInventory.setActive(true);
+        createPhysicalInventory.setCommandId(createPhysicalInventory.getDocumentNumber());
+        physicalInventoryApplicationService.when(createPhysicalInventory);
+    }
+
+    private ProductCommand.CreateProduct createFPProduct_1(String productId) {
+        ProductCommand.CreateProduct prd_1 = new AbstractProductCommand.SimpleCreateProduct();
+        prd_1.setProductId(productId); //Guid.NewGuid().ToString();
+        prd_1.setProductName("Test_" + prd_1.getProductId());
+        prd_1.setPrimaryProductCategoryId(InitAttributeSets.FLUFF_PULP_PRODUCT_CATEGORY_ID);
+        prd_1.setAttributeSetId(InitAttributeSets.FLUFF_PULP_ATTR_SET_ID);//"FluffPulpAttrSet");
+        prd_1.setAttributeSetInstanceId(InitAttributeSets.FP_SECONDARY_QTY_UOM_ADMT_ATTR_SET_INST_ID);
+        prd_1.setIsSerialNumbered(true);
+        prd_1.setQuantityUomId(UomIds.kg);
+        prd_1.setCommandId(UUID.randomUUID().toString());
+
+        productApplicationService.when(prd_1);
+        return prd_1;
+    }
+
+}
