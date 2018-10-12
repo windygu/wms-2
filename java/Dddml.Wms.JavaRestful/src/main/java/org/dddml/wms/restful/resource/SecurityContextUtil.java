0971252eb11fedb4397e79d833add370289259e3
wubuku <yangjiefeng@gmail.com>
2018-10-12 19:22:32 +0800

    增加几行备用代码。

--- java/Dddml.Wms.JavaRestful/src/main/java/org/dddml/wms/restful/resource/SecurityContextUtil.java
+++ java/Dddml.Wms.JavaRestful/src/main/java/org/dddml/wms/restful/resource/SecurityContextUtil.java
@@ -1,5 +1,8 @@
 package org.dddml.wms.restful.resource;
 
+import org.dddml.wms.domain.Command;
+import org.dddml.wms.security.JwtAuthenticationToken;
+import org.dddml.wms.security.JwtUser;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
@@ -13,5 +16,23 @@ public class SecurityContextUtil {
             return SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
         }
         return null;
+    }
+
+    public static void setCommandContext(Command command) {
+        String requesterId = getRequesterId();
+        if (requesterId != null) {
+            command.setRequesterId(requesterId);
+        }
+        JwtAuthenticationToken jwtAuthenticationToken = null;
+        if (SecurityContextHolder.getContext() != null
+                && SecurityContextHolder.getContext().getAuthentication() != null
+                && SecurityContextHolder.getContext().getAuthentication() instanceof JwtAuthenticationToken) {
+            jwtAuthenticationToken = (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        }
+        if (jwtAuthenticationToken != null) {
+            //JwtUser jwtUser = jwtAuthenticationToken.getUser();
+            //String partyId = jwtUser.getPartyId();
+            //command.getCommandContext().put(COMMAND_CONTEXT_PARTY_ID_KEY, partyId);
+        }
     }
 }
