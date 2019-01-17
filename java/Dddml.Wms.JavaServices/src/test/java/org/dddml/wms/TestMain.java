2dc50a5606fb0848855809b4d0012b475b4f9721
wubuku <yangjiefeng@gmail.com>
2019-01-17 11:17:03 +0800

    修改测试代码。

--- java/Dddml.Wms.JavaServices/src/test/java/org/dddml/wms/TestMain.java
+++ java/Dddml.Wms.JavaServices/src/test/java/org/dddml/wms/TestMain.java
@@ -47,13 +47,13 @@ public class TestMain {
         //movementTests_0.testCreateAndCompleteAndReverseMovement();
         //if (true) return;
 
-//        OrderShipGroupTests orderShipGroupTests_0 = new OrderShipGroupTests();
-//        orderShipGroupTests_0.setUp();
-//        //orderShipGroupTests_0.testCreateSOShipGroupAndShipment2();
-//        orderShipGroupTests_0.testCreatePOShipGroupAndShipment2();
-//        //orderShipGroupTests_0.testCreatePOShipGroup1();
-//        //orderShipGroupTests_0.testCreateSOShipGroup1();
-//        if (true) return;
+        //OrderShipGroupTests orderShipGroupTests_0 = new OrderShipGroupTests();
+        //orderShipGroupTests_0.setUp();
+        //orderShipGroupTests_0.testCreateSOShipGroupAndShipment2();
+        ////orderShipGroupTests_0.testCreatePOShipGroupAndShipment2();
+        ////orderShipGroupTests_0.testCreatePOShipGroup1();
+        ////orderShipGroupTests_0.testCreateSOShipGroup1();
+        //if (true) return;
 
         //        ShipmentTests shipmentTests_0 = new ShipmentTests();
         //        shipmentTests_0.setUp();
